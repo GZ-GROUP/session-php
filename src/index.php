01885609690540c354f4b82e7d6450a6b88d23ba
RAGG1911 <148612857+RAGG1911@users.noreply.github.com>
2026-05-14 15:42:28 -0500

    feature: add project structure files, index front end

---
+++ src/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html lang="es" data-theme="customTheme">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesión y Cookies</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Clear+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="hero bg-base-200 min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <div class="flex w-full flex-col">
+ 
+                    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                        <legend class="fieldset-legend">Inicar Sesión</legend>
+
+                        <label class="label">Usuario</label>
+                        <input type="text" class="input" placeholder="Nombre de Usuario" />
+
+                        <label class="label">Contraseña</label>
+                        <input type="password" class="input" placeholder="Contraseña" />
+
+                        <button class="btn btn-neutral mt-4">Ingresar</button>
+                    </fieldset>        
+                    
+                    <label class="toggle text-base-content">
+                        <input type="checkbox" value="synthwave" class="theme-controller" />
+
+                        <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
+
+                        <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
+
+                    </label>
+                    
+                </div>              
+            </div>
+        </div>
+    </div>
+ 
+    
+</body>
+
+</html>
